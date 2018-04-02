0f2e0edc234cecac38d1eb1fe69c9638692c267c
Pedro Rios Fernandez <pedro.rios@ctag.com>
2018-04-02 16:27:20 +0200

    [ADD] New activities, only the file

---
+++ app/src/main/java/peter/skydev/quickest/Result.kt
@@ -0,0 +1,12 @@
+package peter.skydev.quickest
+
+import android.app.Activity
+import android.os.Bundle
+
+class Result : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
