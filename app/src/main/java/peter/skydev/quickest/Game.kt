8ce1146854c3edf04f0db5fc3b8270e30e5449ea
Pedro Rios Fernandez <pedro.rios@ctag.com>
2018-04-05 15:42:06 +0200

    [ADD] Chronometer in game activity

--- app/src/main/java/peter/skydev/quickest/Game.kt
+++ app/src/main/java/peter/skydev/quickest/Game.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.SystemClock
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_game.*
 import org.jetbrains.anko.alert
@@ -17,6 +18,12 @@ class Game : Activity() {
     private var arrScore = LongArray(numIter)
     private var globalText: String = ""
 
+    private var customHandler = Handler()
+    private var startTime = 0L
+    private var timeToMilliSeconds = 0L
+    private var timeSwapBuff = 0L
+    private var updateTime = 0L
+
     private fun init() {
         this.start = 0
         this.red = false
@@ -26,6 +33,20 @@ class Game : Activity() {
         textToClick.setOnClickListener {
             if (!red)
                 alert(resources.getText(R.string.gameMSGError).toString()) {}.show()
+
+            // TODO Stop play or restart it
+        }
+    }
+
+    var updateTimeThread: Runnable = object : Runnable {
+        override fun run() {
+            timeToMilliSeconds = SystemClock.uptimeMillis() - startTime
+            updateTime = timeSwapBuff + timeToMilliSeconds
+            var secs = updateTime / 1000
+            secs %= 60
+            var milliseconds = updateTime % 1000
+            timerValue.text = "" + String.format("%2d", secs) + "." + String.format("%3d", milliseconds)
+            customHandler.postDelayed(this, 0)
         }
     }
 
@@ -47,14 +68,22 @@ class Game : Activity() {
             red = true
             textToClick.setBackgroundColor(resources.getColor(R.color.colorRed))
             start = System.nanoTime()
+
+            startTime = SystemClock.uptimeMillis()
+            customHandler.postDelayed(updateTimeThread, 0)
+
             textToClick.setOnClickListener {
                 textToClick.setOnClickListener {
                     if (!red)
                         alert(resources.getText(R.string.gameMSGError).toString()) {}.show()
+
+                    // TODO Stop play or restart it
                 }
 
                 val elapsedTime = System.nanoTime() - start
                 Log.d(this.TAG, "Tiempo Total: " + elapsedTime)
+
+                customHandler.removeCallbacks(updateTimeThread)
 
                 this.globalText = globalText + "\n" + numActualIter + ": " + elapsedTime
 
