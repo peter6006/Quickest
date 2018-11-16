992cb0e3ce3cf95e4dab118e9739ff5b7ff79471
PEDRO RIOS FERNANDEZ <pedro.rios@ctag.com>
2018-11-16 12:17:56 +0100

    Changed layouts and name control

--- app/src/main/java/peter/skydev/quickest/MainActivity.kt
+++ app/src/main/java/peter/skydev/quickest/MainActivity.kt
@@ -2,12 +2,16 @@ package peter.skydev.quickest
 
 import android.Manifest
 import android.accounts.AccountManager
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.util.Patterns
+import android.view.View
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
@@ -15,6 +19,7 @@ import io.vrinda.kotlinpermissions.PermissionCallBack
 import io.vrinda.kotlinpermissions.PermissionsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
+import org.jetbrains.anko.toast
 import java.util.regex.Pattern
 
 
@@ -23,6 +28,10 @@ class MainActivity : PermissionsActivity() {
     private val TAG = "MainActivity"
     private lateinit var mAuth: FirebaseAuth
     private lateinit var userDataDB: DatabaseReference
+    private lateinit var userNamesDB: DatabaseReference
+    private lateinit var userNamesDataSnapshot: DataSnapshot
+    var userName: String = ""
+
     private lateinit var db: FirebaseDatabase
     var currentUser: FirebaseUser? = null
 
@@ -33,39 +42,38 @@ class MainActivity : PermissionsActivity() {
         mAuth = FirebaseAuth.getInstance()
         this.currentUser = mAuth!!.currentUser
 
-        if (android.os.Build.VERSION.SDK_INT >= 23) {
-            val permissionCheck = ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.GET_ACCOUNTS)
-            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-                Log.d(TAG, "Permission denied")
-                requestPermissions(android.Manifest.permission.GET_ACCOUNTS, object : PermissionCallBack {
-                    override fun permissionGranted() {
-                        super.permissionGranted()
-                        Log.v("Call permissions", "Granted")
-
-                        signInOrLogIn()
-                    }
-
-                    override fun permissionDenied() {
-                        super.permissionDenied()
-                        Log.v("Call permissions", "Denied")
-                        alert("We only need your email for leaderboard purposes", "Permission") {
-                            yesButton {}
-                        }.show()
-                    }
-                })
-            } else {
-                Log.d(TAG, "Permission granted")
-                signInOrLogIn()
-            }
-        }else{
-            signInOrLogIn()
-        }
+        askForPermission()
 
         if (currentUser != null) {
             setListeners()
         }
 
+        tvYourName.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+            val inflater = this.layoutInflater
+            val dialogView = inflater.inflate(R.layout.custom_dialog, null)
+            dialogBuilder.setView(dialogView)
+
+            val editText = dialogView.findViewById<View>(R.id.editTextName) as EditText
+
+            dialogBuilder.setMessage(resources.getString(R.string.resultAddName))
+            dialogBuilder.setPositiveButton("Save", DialogInterface.OnClickListener { dialog, whichButton ->
+                if (!userNamesDataSnapshot.hasChild(editText.text.toString())) {
+                    userDataDB.child(currentUser!!.uid).child("UserName").setValue(editText.text.toString())
+
+                    userNamesDB.child(userName).removeValue()
+                    userNamesDB.child(editText.text.toString()).setValue(currentUser!!.uid)
+                } else {
+                    toast(resources.getString(R.string.resultAddNameExists))
+                    showNewNameDialog()
+                }
+            })
+            dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, whichButton ->
+                //pass
+            })
+            val b = dialogBuilder.create()
+            b.show()
+        }
 
         gameButton.setOnClickListener {
             val intent = Intent(this, Game::class.java)
@@ -83,10 +91,48 @@ class MainActivity : PermissionsActivity() {
         }
     }
 
-    private fun signInOrLogIn(){
+    private fun askForPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            val permissionCheck = ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.GET_ACCOUNTS)
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                Log.d(TAG, "Permission denied")
+                requestPermissions(android.Manifest.permission.GET_ACCOUNTS, object : PermissionCallBack {
+                    override fun permissionGranted() {
+                        super.permissionGranted()
+                        Log.v("Call permissions", "Granted")
+
+                        signInOrLogIn()
+                    }
+
+                    override fun permissionDenied() {
+                        super.permissionDenied()
+                        Log.v("Call permissions", "Denied")
+                        alert("We only need your email for leaderboard purposes", "Permission") {
+                            yesButton {
+                                askForPermission()
+                            }
+                        }.show()
+                    }
+                })
+            } else {
+                Log.d(TAG, "Permission granted")
+                signInOrLogIn()
+            }
+        } else {
+            signInOrLogIn()
+        }
+    }
+
+    private fun signInOrLogIn() {
         mAuth.createUserWithEmailAndPassword(getEmail(), "passwordDefault")
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
+                        FirebaseDatabase.getInstance()
+                        db = FirebaseDatabase.getInstance()
+                        userDataDB = db.getReference("UserData")
+                        userNamesDB = db.getReference("UserNames")
+
                         Log.d(TAG, "User created successfully!")
                         currentUser = mAuth!!.currentUser
                         userDataDB.child(currentUser!!.uid).child("UserEmail").setValue(currentUser!!.email)
@@ -95,9 +141,16 @@ class MainActivity : PermissionsActivity() {
                         userDataDB.child(currentUser!!.uid).child("UserName").setValue("")
 
                         setListeners()
+
+                        showNewNameDialog()
                     } else {
                         Log.d(TAG, "There was an error creating the user")
                         if (task.exception.toString().contains("The email address is already in use by another account")) {
+                            FirebaseDatabase.getInstance()
+                            db = FirebaseDatabase.getInstance()
+                            userDataDB = db.getReference("UserData")
+                            userNamesDB = db.getReference("UserNames")
+
                             mAuth.signInWithEmailAndPassword(getEmail(), "passwordDefault").addOnCompleteListener { result ->
                                 if (result.isSuccessful) {
                                     currentUser = mAuth!!.currentUser
@@ -113,6 +166,7 @@ class MainActivity : PermissionsActivity() {
         FirebaseDatabase.getInstance()
         db = FirebaseDatabase.getInstance()
         userDataDB = db.getReference("UserData")
+        userNamesDB = db.getReference("UserNames")
 
         userDataDB.child(currentUser!!.uid).child("UserScore").addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError?) {}
@@ -127,6 +181,23 @@ class MainActivity : PermissionsActivity() {
 
             override fun onDataChange(p0: DataSnapshot?) {
                 tvYourPosition.text = p0!!.value.toString()
+            }
+        })
+
+        userDataDB.child(currentUser!!.uid).child("UserName").addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {}
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                tvYourName.setText(p0!!.value.toString() + ":")
+                userName = p0!!.value.toString()
+            }
+        })
+
+        userNamesDB.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {}
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                userNamesDataSnapshot = p0!!
             }
         })
     }
@@ -149,5 +220,32 @@ class MainActivity : PermissionsActivity() {
             return "error@error.com"
         }
         return "error@error.com"
+    }
+
+    fun showNewNameDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val editText = dialogView.findViewById<View>(R.id.editTextName) as EditText
+
+        dialogBuilder.setMessage(resources.getString(R.string.resultAddName))
+        dialogBuilder.setPositiveButton("Save", DialogInterface.OnClickListener { dialog, whichButton ->
+            if (!userNamesDataSnapshot.hasChild(editText.text.toString())) {
+
+                userDataDB.child(currentUser!!.uid).child("UserName").setValue(editText.text.toString())
+
+                userNamesDB.child(editText.text.toString()).setValue(currentUser!!.uid)
+            } else {
+                toast(resources.getString(R.string.resultAddNameExists))
+                showNewNameDialog()
+            }
+        })
+        dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, whichButton ->
+            //pass
+        })
+        val b = dialogBuilder.create()
+        b.show()
     }
 }
